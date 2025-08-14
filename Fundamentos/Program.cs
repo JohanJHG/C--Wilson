40fed16738e32119f3c227632f79550b6dc8834b
Johan Sebastian Hernandez <jhoan.rhz@gmail.com>
2025-08-14 06:24:47 -0500

    Tipos de datos, array listas y diccionarios

---
+++ Fundamentos/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class Fundamentos
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tipos de datos");
+            Console.WriteLine("int: " + 10);
+            Console.WriteLine("decimal: " + 10.5);
+            Console.WriteLine("string: " + "Hola");
+            Console.WriteLine("bool: " + true);
+            Console.WriteLine("DateTime: " + DateTime.Now);
+            Console.WriteLine("TimeSpan: " + DateTime.Now.TimeOfDay);
+            Console.WriteLine("Minuto: " + DateTime.Now.Minute);
+            Console.WriteLine("Segundo: " + DateTime.Now.Second);
+            Console.WriteLine("Caracter: " + 'A');
+
+            // Arrays, Listas y Diccionarios
+            int[] numeros = { 1, 2, 3, 4, 5 };
+            List<string> nombres = new List<string> { "Juan", "Maria", "Pedro" };
+            Dictionary<string, int> edades = new Dictionary<string, int>
+            {
+                { "Juan", 30 },
+                { "Maria", 25 },
+                { "Pedro", 40 }
+            };
+        }
+    }
+}
